2acabfbae83cfd929a1ad33b5e3b235ecc452b68
osboxes <vcljs2013@gmail.com>
2017-10-22 12:59:04 +0100

    snore changes

--- snore_classifier.py
+++ snore_classifier.py
@@ -10,7 +10,15 @@ import numpy
 # Training Step: 544  | total loss: 0.15866
 # | Adam | epoch: 034 | loss: 0.15866 - acc: 0.9818 -- iter: 0000/1000
 
-batch=speech_data.wave_batch_snore(140)
+def validateWav(demo_file):
+    demo = speech_data.load_wav_file(speech_data.snore_train_path + demo_file,70000,1)
+    result = model.predict([demo])
+    result = numpy.argmax(result)
+    print("predicted digit for %s : result = %d " % (demo_file, result))
+
+
+
+batch=speech_data.wave_batch_snore(120)
 X,Y=next(batch)
 
 
@@ -20,15 +28,29 @@ number_classes=2 # Digits
 tflearn.init_graph(num_cores=8, gpu_memory_fraction=0.5)
 
 net = tflearn.input_data(shape=[None, 140000])
-net = tflearn.fully_connected(net, 128)
-net = tflearn.fully_connected(net, 128)
+net = tflearn.fully_connected(net, 248)
+net = tflearn.fully_connected(net, 64)
 net = tflearn.dropout(net, 0.5)
 net = tflearn.fully_connected(net, number_classes, activation='softmax')
 net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
 
 model = tflearn.DNN(net)
-for i in range(1,10) :
-    model.fit(X, Y,n_epoch=5,show_metric=True,snapshot_step=100)
+for i in range(1,500) :
+    model.fit(X, Y,n_epoch=25,show_metric=True,snapshot_step=100)
     X, Y = next(batch)
+    validateWav("nosnore3.wav")
+    validateWav("nosnore4.wav")
+    validateWav("snore1.wav")
+    validateWav("snore2.wav")
 
-model.save("mode/snore")
+model.save("model/snore")
+model.load("model/snore")
+
+validateWav("nosnore1.wav")
+validateWav("nosnore2.wav")
+validateWav("nosnore3.wav")
+validateWav("nosnore4.wav")
+validateWav("snore1.wav")
+validateWav("snore2.wav")
+validateWav("snore3.wav")
+validateWav("snore4.wav")
