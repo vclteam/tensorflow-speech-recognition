885026e982a02e2b95fb913b3036f2b3f55ee81a
VCL.JS <vcljs2013@gmail.com>
2017-10-31 17:42:13 +0200

    animal

--- snore_classifier.py
+++ snore_classifier.py
@@ -21,7 +21,7 @@ batch=speech_data.wave_batch_snore(128,True)
 testbatch=speech_data.wave_batch_snore(30,True)
 
 learning_rate = 0.001
-number_classes=2 # Digits
+number_classes = 2 # Digits
 
 # Classification
 tflearn.init_graph(num_cores=5, gpu_memory_fraction=0.6)
@@ -29,13 +29,21 @@ tflearn.init_graph(num_cores=5, gpu_memory_fraction=0.6)
 width = 40
 height = 200
 convnet = tflearn.input_data(shape=[None, width, height, 1], name='input')
-convnet = tflearn.conv_2d(convnet, 32, 32, activation='relu')
-convnet = tflearn.max_pool_2d(convnet, 16,16)
-convnet = tflearn.conv_2d(convnet, 16, 16, activation='relu')
-convnet = tflearn.max_pool_2d(convnet, 8,8)
+convnet = tflearn.conv_2d(convnet, 128, 64, activation='relu')
+convnet = tflearn.conv_2d(convnet, 128, 64, activation='relu')
+convnet = tflearn.max_pool_2d(convnet, 2,2)
+convnet = tflearn.dropout(convnet, 0.5)
 
-convnet = tflearn.fully_connected(convnet, 4096, activation='softmax')
-convnet = tflearn.fully_connected(convnet, 1024, activation='softmax')
+convnet = tflearn.conv_2d(convnet, 128, 64, activation='relu')
+convnet = tflearn.conv_2d(convnet, 128, 64, activation='relu')
+convnet = tflearn.max_pool_2d(convnet, 2, 2)
+convnet = tflearn.dropout(convnet, 0.5)
+
+convnet = tflearn.conv_2d(convnet, 128, 64, activation='relu')
+convnet = tflearn.conv_2d(convnet, 128, 64, activation='relu')
+convnet = tflearn.max_pool_2d(convnet, 2, 2)
+convnet = tflearn.dropout(convnet, 0.5)
+
 convnet = tflearn.fully_connected(convnet, 2, activation='softmax')
 convnet = tflearn.regression(convnet, optimizer='adam',  loss='categorical_crossentropy', name='targets')
 
@@ -44,28 +52,28 @@ convnet = tflearn.regression(convnet, optimizer='adam',  loss='categorical_cross
 #    tf.add_to_collection(tf.GraphKeys.VARIABLES, x )
 
 model = tflearn.DNN(convnet,tensorboard_verbose=0)
-try :
+try:
     if len(os.listdir("model/"))>0 :
         model.load("model/snore")
-except :
-    print ("NO DATA")
+except:
+    print("NO DATA")
 
 
 for i in range(300) :
     X, Y = next(batch)
     Xtest, Ytest = next(testbatch)
 
-    res = model.fit(X, Y,validation_set=(Xtest,Ytest),n_epoch=50,show_metric=True)
+    res = model.fit(X ,Y ,validation_set=(Xtest,Ytest) ,n_epoch=50 ,show_metric=True)
 
-    nr1=validateWav("nosnore1.wav")
-    nr2 =validateWav("nosnore2.wav")
-    nr3=validateWav("nosnore3.wav")
-    nr4 =validateWav("nosnore4.wav")
+    nr1 = validateWav("nosnore1.wav")
+    nr2 = validateWav("nosnore2.wav")
+    nr3 = validateWav("nosnore3.wav")
+    nr4 = validateWav("nosnore4.wav")
 
-    r1 =validateWav("snore1.wav")
-    r2 =validateWav("snore2.wav")
-    r3 =validateWav("snore3.wav")
-    r4 =validateWav("snore4.wav")
+    r1 = validateWav("snore1.wav")
+    r2 = validateWav("snore2.wav")
+    r3 = validateWav("snore3.wav")
+    r4 = validateWav("snore4.wav")
 
     model.save("model/snore")
 
